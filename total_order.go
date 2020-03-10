023cf51d095bf46790050a03d6c1897c9d29af38
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2020-03-10 15:34:28 +0800

    MVP done

--- total_order.go
+++ total_order.go
@@ -8,24 +8,20 @@ import (
 
 func main() {
 
-
 	s := NewServer()
-	clients := []client {}
 
-
-	for i := 1;  i<=5; i++ {
+	for i := 1; i <= 5; i++ {
 		c := NewClient(fmt.Sprintf("Client %d", i), *s)
 		c.registerWithServer(*s)
-		clients = append(clients, *c)
-		s.clientChannels = append(s.clientChannels, c.clientChannel)
+		s.clientArray = append(s.clientArray, *c)
 
 	}
 
-	for _, c := range clients {
+	for _, c := range s.clientArray {
 		go c.listen()
 	}
 
-	for _, c := range clients {
+	for _, c := range s.clientArray {
 		go c.ping()
 	}
 
@@ -36,88 +32,107 @@ func main() {
 type server struct {
 	channel chan message
 	//clientChannels []chan message
-	clients []client
-
+	clientArray []client
+	logicalTS int
 }
 
 type message struct {
-	senderName string
+	senderName    string
 	messageString string
+	logicalTS int
 }
 
-func NewServer() *server{
-	channel := make(chan message)
-	clientArray := []client {}
+type client struct {
+	name          string
+	clientChannel chan message
+	server        server
+	logicalTS int
+}
 
-	s := server{channel, clientArray}
+func NewServer() *server {
+	channel := make(chan message)
+	clientArray := []client{}
+
+	s := server{channel, clientArray, 0}
 
 	return &s
 }
 
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func sendMessage(clientChannel chan message, msg message) {
+	const MAX_DELAY float32 = 5
+	//random time delay
+	var numSeconds float32 = rand.Float32() * MAX_DELAY
+	fmt.Printf("Simulated Network Latency %f seconds \n", numSeconds)
+	time.Sleep(time.Duration(numSeconds) * time.Second)
+
+	clientChannel <- msg
+}
+
 func (s server) listen() {
+
 	for {
-		//random time delay
-		var max float32 = 5
-		var numSeconds float32 = rand.Float32() * max
-		fmt.Printf("Server delay %f seconds \n", numSeconds)
-		time.Sleep(time.Duration(numSeconds) * time.Second)
+		msg := <-s.channel
+		s.logicalTS = Max(s.logicalTS, msg.logicalTS) + 1
+		fmt.Printf("[TS: %d] Server: Received <%s> from client <%s> \n", s.logicalTS, msg.messageString, msg.senderName)
 
-		msg := <- s.channel
-		fmt.Printf("Server: Received <%s> from client \n", msg)
+		//advance the timestamp of the msg
+		msg.logicalTS = s.logicalTS
 
-		for _, channel := range s.clientChannels {
-
-			channel <- msg
+		//Server broadcasting
+		for _, client := range s.clientArray {
+			if client.name == msg.senderName {
+				continue
+			} else {
+				go sendMessage(client.clientChannel, msg)
+			}
 
 		}
 	}
 }
 
-func (s *server) addClientChannel(c client) []chan message{
+func (s *server) addClient(c client) []client {
 	fmt.Printf("Registering %s \n", c.name)
-	s.clientChannels = append(s.clientChannels, c.clientChannel)
-	return s.clientChannels
+	s.clientArray = append(s.clientArray, c)
+	return s.clientArray
 }
 
-type client struct {
-	name string
-	clientChannel chan message
-	server server
-}
-
-
-
-// create a channel
+// Constructor for client
 func NewClient(name string, s server) *client {
 
 	clientChannel := make(chan message)
-	c := client{name, clientChannel, s}
+	c := client{name, clientChannel, s, 0}
 	return &c
 
 }
 
-func (c client) registerWithServer(s server){
+func (c client) registerWithServer(s server) {
 	c.server = s
-	s.clientChannels = s.addClientChannel(c)
+	s.clientArray = s.addClient(c)
 }
 
-func (c client) ping(){
-	var clientMessage message;
+func (c client) ping() {
+	var clientMessage message
 	for {
 		time.Sleep(2 * time.Second)
-		clientMessage = message{c.name, fmt.Sprintf("Hello, I am %s", c.name)}
+		clientMessage = message{c.name, fmt.Sprintf("Hello, I am %s", c.name), c.logicalTS}
 		c.server.channel <- clientMessage
 	}
 
 }
 
-func (c client) listen(){
+func (c client) listen() {
 	for {
-		broadCastedMessage:= <- c.clientChannel
-		fmt.Printf("Client %s: Received '%s' from server \n", c.name, broadCastedMessage)
+		broadCastedMessage := <-c.clientChannel
+		//Modify Logical Timestamp
+		c.logicalTS = Max(c.logicalTS, broadCastedMessage.logicalTS) + 1
+		fmt.Printf("[TS: %d] Client %s: Received '%s' from server \n", c.logicalTS, c.name, broadCastedMessage.messageString)
 	}
 
 }
-
-
-

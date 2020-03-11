bafff49e2d0a939746d061688183d4c66948272d
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2020-03-11 19:01:49 +0800

    the key is not to close the channel prematurely. what if we use a quit channel?

--- total_order.go
+++ total_order.go
@@ -7,10 +7,10 @@ import (
 	"time"
 )
 
-const NUMBER_OF_MESSAGES_TO_SEND int = 10
-const NUMBER_OF_CLIENTS int = 10
+const NUMBER_OF_MESSAGES_TO_SEND int = 3
+const NUMBER_OF_CLIENTS int = 5
 const CLIENT_PING_INTERVAL float32 = 2
-const MAX_NETWORK_DELAY float32 = 5
+const MAX_NETWORK_DELAY float32 = 2
 
 type server struct {
 	channel chan message
@@ -52,17 +52,25 @@ func main() {
 		s.clientArray = append(s.clientArray, *c)
 
 	}
+	var allMessages chan message =  make(chan message, NUMBER_OF_CLIENTS * NUMBER_OF_CLIENTS * NUMBER_OF_MESSAGES_TO_SEND)
 
 	for _, c := range s.clientArray {
 		fmt.Println(c.name)
 		go c.timePing()
-		go c.pingAndListen(wg)
+		go c.pingAndListen(allMessages)
 
 	}
 
-	s.listen()
+	s.listen(allMessages)
 
 	fmt.Println("ALL DONE!")
+	fmt.Println(len(allMessages))
+	messageArray := []message{}
+	for message := range allMessages {
+		messageArray = append(messageArray, message)
+	}
+
+	fmt.Println(len(messageArray))
 
 }
 
@@ -83,19 +91,24 @@ func Max(x, y int) int {
 }
 
 func sendMessage(clientChannel chan message, msg message) {
-
+	fmt.Println("send message", msg.messageString, msg.senderName)
 	//random time delay
 	var numSeconds float32 = rand.Float32() * MAX_NETWORK_DELAY
 	//fmt.Printf("Simulated Network Latency %f seconds \n", numSeconds)
 	time.Sleep(time.Duration(numSeconds) * time.Second)
+	//fmt.Println(msg.messageString)
+	//defer wg.Done()
 	clientChannel <- msg
+
+	return
 }
 
-func (s server) listen() {
-
+func (s server) listen(allMessages chan message) {
+	//wg := sync.WaitGroup{}
 	var numChannelsPinging int = NUMBER_OF_CLIENTS
 	for {
 		msg := <-s.channel
+		//allMessages <- msg
 		s.logicalTS = Max(s.logicalTS, msg.logicalTS) + 1
 		fmt.Printf("[TS: %d] Server: Received <%s> from client <%s> \n", s.logicalTS, msg.messageString, msg.senderName)
 
@@ -105,6 +118,7 @@ func (s server) listen() {
 				continue
 			} else {
 				newMessage := message{"Server", fmt.Sprintf("[Forwarded] %s", msg.messageString), s.logicalTS}
+				//wg.Add(1)
 				go sendMessage(client.clientChannel, newMessage)
 			}
 
@@ -113,17 +127,22 @@ func (s server) listen() {
 		if msg.messageString == "LAST" {
 			fmt.Printf("LAST from %s \n", msg.senderName)
 			numChannelsPinging -= 1
+			fmt.Println("NUM CHANNELS: ", numChannelsPinging)
+
 			if numChannelsPinging == 0 {
-				time.Sleep(time.Duration(MAX_NETWORK_DELAY) * time.Second * 2)
-				for _, client := range s.clientArray {
-					fmt.Printf("Server closing %s's channel \n", client.name)
-					close(client.clientChannel)
-				}
+				//wg.Wait()
+				time.Sleep(time.Duration(MAX_NETWORK_DELAY) * time.Second)
+				//for _, client := range s.clientArray {
+				//	fmt.Printf("Server closing %s's channel \n", client.name)
+					//time.Sleep(10 * time.Second)
+					//close(client.clientChannel)
 				return
+				}
+
 			}
 		}
 	}
-}
+
 
 func (s *server) addClient(c client) []client {
 	fmt.Printf("Registering %s \n", c.name)
@@ -152,20 +171,22 @@ func (c client) timePing() {
 		time.Sleep(time.Duration(CLIENT_PING_INTERVAL) * time.Second)
 		c.readyChannel <- i
 	}
-	close(c.readyChannel)
+	//close(c.readyChannel)
 	return
 }
 
-func (c client) pingAndListen(wg sync.WaitGroup) {
+func (c client) pingAndListen(allMessages chan message) {
 	var clientMessage message
 
 	for {
 		select {
+
 		case broadcastedMessage := <-c.clientChannel:
+			//allMessages <- broadcastedMessage
 			c.logicalTS = Max(c.logicalTS, broadcastedMessage.logicalTS) + 1
 			fmt.Printf("[TS: %d] %s: Received '%s' from %s \n", c.logicalTS, c.name, broadcastedMessage.messageString, broadcastedMessage.senderName)
 		case messageNo := <-c.readyChannel:
-			fmt.Printf("%s is pinging now at TS: %d \n", c.name, c.logicalTS)
+			fmt.Printf("%s is pinging now at TS: %d for message %d \n", c.name, c.logicalTS, messageNo)
 			c.logicalTS += 1
 			if messageNo == NUMBER_OF_MESSAGES_TO_SEND {
 				clientMessage = message{c.name, "LAST", c.logicalTS}
@@ -173,6 +194,7 @@ func (c client) pingAndListen(wg sync.WaitGroup) {
 				clientMessage = message{c.name, fmt.Sprintf("Hello %d from %s", messageNo, c.name), c.logicalTS}
 			}
 			c.server.channel <- clientMessage
+		default:
 
 		}
 	}
